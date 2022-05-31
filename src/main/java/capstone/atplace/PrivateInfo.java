8dd8e4fc2e95db4665b769c0c447156f5c1a81b6
노희재 <80248626+HEEJAERO@users.noreply.github.com>
2022-05-31 15:48:03 +0900

    Delete PrivateInfo.java

--- src/main/java/capstone/atplace/PrivateInfo.java
+++
@@ -1,9 +0,0 @@
-package capstone.atplace;
-
-import lombok.Getter;
-
-@Getter
-public class PrivateInfo {
-    private String API_URL = "https://dapi.kakao.com/v2/local/search/address.json?analyze_type=similar&page=1&size=10&query=";
-    private String API_KEY = "KakaoAK bc6a29f2bfa0a8ccd02aeb329145a454";
-}
